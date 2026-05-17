4b3b134eae5f2a0e4adf817bca32dc4adf989832
Mike <mperniaponce2@alumnos.cei.es>
2026-05-17 21:17:41 +0200

    fix: CORS petition failing on resources reading

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ const tagsRoutes = require('./src/routes/tags.routes');
 const requestsRoutes = require('./src/routes/requests.routes');
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  next();
+});
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
