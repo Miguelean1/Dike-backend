1d0db37504fa699109daabd2edb4828b01f16552
Mike <mperniaponce2@alumnos.cei.es>
2026-05-27 21:24:33 +0200

    hotfix:add production config to sequelize

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -1,13 +1,16 @@
 require('dotenv').config({ path: require('path').join(__dirname, '../../.env') });
 
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  dialect: 'mysql',
+  logging: false,
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    dialect: 'mysql',
-    logging: false,
-  },
+  development: config,
+  production: config,
 };
